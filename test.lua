94a77eaecbad92fb683d9db0294d7acf3308a934
zhouyan <zhouyan@onemt.com.cn>
2017-11-10 20:10:17 +0800

    学习 Sproto ， 增加一些测试代码

--- test.lua
+++ test.lua
@@ -23,14 +23,49 @@ local sp = sproto.parse [[
 ]]
 
 -- core.dumpproto only for debug use
+print(type(sp))
+print_r(sp)
+print('-----------111-----dump-proto--VVV---')
+
 core.dumpproto(sp.__cobj)
+
+print("-------------222------AAA------")
 
 local def = sp:default "Person"
 print("default table for Person")
 print_r(def)
-print("--------------")
 
-local ab = {
+--[[
+local def = sp:default("Person", "REQUEST")
+print("default table for Person.REQUEST")
+print_r(def)
+
+local def = sp:default("Person", "RESPONSE")
+print("default table for Person")
+print_r(def)
+]]
+
+local def = sp:default "AddressBook"
+print("default table for AddressBook")
+print_r(def)
+
+
+--[[
+
+
+local def = sp:default("foobar", "REQUEST")
+print("default table for foobar.REQUEST")
+print_r(def)
+
+local def = sp:default("foobar", "RESPONSE")
+print("default table for foobar.RESPONSE")
+print_r(def)
+]]
+
+
+print("-------------333------------")
+
+local addressBook = {
 	person = {
 		[10000] = {
 			name = "Alice",
@@ -38,11 +73,13 @@ local ab = {
 			phone = {
 				{ number = "123456789" , type = 1 },
 				{ number = "87654321" , type = 2 },
-			}
+			},
+			email = "zhouyanlt@qq.com",
 		},
 		[20000] = {
 			name = "Bob",
 			id = 20000,
+			email = "zhouyan@qq.com",
 			phone = {
 				{ number = "01234567890" , type = 3 },
 			}
@@ -61,6 +98,11 @@ local ab = {
 
 collectgarbage "stop"
 
-local code = sp:encode("AddressBook", ab)
+-- 序列化： lua table ---> sproto bin
+local code = sp:encode("AddressBook", addressBook)
+print("-----------------------lua table ---> sproto bin----------------------\n", type(code))
+print(code:byte(1, code:len()))
+print(code)
+-- 反序列化： sproto bin ---> lua table
 local addr = sp:decode("AddressBook", code)
 print_r(addr)
--- testrpc.lua
+++ testrpc.lua
@@ -1,4 +1,5 @@
 local sproto = require "sproto"
+local core = require "sproto.core"
 local print_r = require "print_r"
 
 local server_proto = sproto.parse [[
@@ -37,62 +38,111 @@ local client_proto = sproto.parse [[
 }
 ]]
 
+print_r(server_proto)
+core.dumpproto(server_proto.__cobj)
 
-assert(server_proto:exist_type "package")
-assert(server_proto:exist_proto "foobar")
+print_r(client_proto)
+core.dumpproto(client_proto.__cobj)
 
-print("=== default table")
+print("\n\n-------------------11111--------------------------")
+
+print(server_proto:exist_type "package")
+print(server_proto:exist_proto "foobar")
+print(server_proto:exist_proto "zhouyan")
+
+print("\n\n================= default table=================")
 
 print_r(server_proto:default("package"))
 print_r(server_proto:default("foobar", "REQUEST"))
-assert(server_proto:default("foo", "REQUEST")==nil)
-assert(server_proto:request_encode("foo")=="")
-server_proto:response_encode("foo", { ok = true })
-assert(server_proto:request_decode("blackhole")==nil)
-assert(server_proto:response_decode("blackhole")==nil)
+print_r(server_proto:default("foobar.request"))
 
-print("=== test 1")
+assert(server_proto:default("foo", "REQUEST")==nil)
+
+print("\n\n================= encode =================")
+local foo_request_encode = server_proto:request_encode("foo")
+print('request_encode("foo")', type(foo_request_encode), foo_request_encode:len(), tostring(foo_request_encode)) -- == ""
+
+local foo_response_encode = server_proto:response_encode("foo", { ok = true })
+print('response_encode("foo", { ok = true })', type(foo_response_encode), foo_response_encode:len(), tostring(foo_response_encode))
+print( foo_response_encode:byte(1, foo_response_encode:len()) )
+
+print("\n\n================= decode =================")
+local blackhole_req_decode = server_proto:request_decode("blackhole")
+print(type(blackhole_req_decode), tostring(blackhole_req_decode), blackhole_req_decode) -- nil
+
+local blackhole_res_decode = server_proto:response_decode("blackhole")
+print(type(blackhole_res_decode), tostring(blackhole_res_decode), blackhole_res_decode) -- nil
+
+print("\n\n====================================== test 1")
 
 -- The type package must has two field : type and session
 local server = server_proto:host "package"
 local client = client_proto:host "package"
+print_r(server)
+print_r(client)
+
 local client_request = client:attach(server_proto)
 
+print("\n\n第一次通讯， session = 1")
 print("client request foobar")
 local req = client_request("foobar", { what = "foo" }, 1)
+-- CS程序内，req 要发给服务端
 print("request foobar size =", #req)
-local type, name, request, response = server:dispatch(req)
-assert(type == "REQUEST" and name == "foobar")
+print(type(req), req:byte(1, req:len()))
+print("---------111-----------")
+
+print("服务端收到客户端请求的 req 后开始处理：")
+local proto_type, name, request, response = server:dispatch(req)
+print(proto_type, name, request, response)
+assert(proto_type == "REQUEST" and name == "foobar")
 print_r(request)
+print("\n----------222----------")
+
+print("服务端返回 resp")
 print("server response")
 local resp = response { ok = true }
 print("response package size =", #resp)
-print("client dispatch")
-local type, session, response = client:dispatch(resp)
-assert(type == "RESPONSE" and session == 1)
-print_r(response)
+print(type(resp)) -- string, CS程序内，resp 要发给客户端
+print(resp:byte(1, resp:len()))
 
+print("客户端收到服务端返回的 resp 后开始处理：")
+print("client dispatch")
+local proto_type, session, response = client:dispatch(resp)
+assert(proto_type == "RESPONSE" and session == 1)
+print_r(response) -- string
+
+
+print("\n\n第二次通讯， session = 2")
 local req = client_request("foo", nil, 2)
 print("request foo size =", #req)
-local type, name, request, response = server:dispatch(req)
-assert(type == "REQUEST" and name == "foo" and request == nil)
+local _type, name, request, response = server:dispatch(req)
+assert(_type == "REQUEST" and name == "foo" and request == nil)
 local resp = response { ok = false }
 print("response package size =", #resp)
 print("client dispatch")
-local type, session, response = client:dispatch(resp)
-assert(type == "RESPONSE" and session == 2)
+local _type, session, response = client:dispatch(resp)
+assert(_type == "RESPONSE" and session == 2)
 print_r(response)
 
+print("\n\n第三次通讯， session = 3")
 local req = client_request("bar", nil, 3)
 print("request bar size =", #req)
-local type, name, request, response = server:dispatch(req)
-assert(type == "REQUEST" and name == "bar" and request == nil)
-assert(select(2,client:dispatch(response())) == 3)
+local _type, name, request, response = server:dispatch(req)
+assert(_type == "REQUEST" and name == "bar" and request == nil)
+local resp = response()
+print(type(resp), resp:len(), resp:byte(1, resp:len()))
+local _type, session, response = client:dispatch(resp)
+print(_type, session, response)
 
+-- 这里相当于 assert(session == 3)
+--assert(select(2,client:dispatch(response())) == 3)
+
+print("\n\n==========blackhole============")
 local req = client_request "blackhole"	-- no response
+print(req, req:byte(1, req:len()))
 print("request blackhole size = ", #req)
 
-print("=== test 2")
+print("\n\n======================= test 2")
 local v, tag = server_proto:request_encode("foobar", { what = "hello"})
 assert(tag == 1)	-- foobar : 1
 print("tag =", tag)
